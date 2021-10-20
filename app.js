3934b0fe1609eddd96e2a7c96348f8cde41a554b
lettuce <lettuce.lu@lettucelu.com>
2021-10-20 22:20:59 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars');
+// 引用 body-parser
+const bodyParser = require('body-parser')
 
 const Todo = require('./models/todo')
 const app = express()
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
 // connect to the mongoDB
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -35,6 +38,16 @@ app.get('/', (req, res) => {
 })
 
 
+app.get('/todos/new', (req, res) => {
+  res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  return Todo.create({ name })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+})
 // setting up port 3000 for service
 app.listen('3000', () => {
   console.log('App is running on http://localhost:3000')
