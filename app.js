e820067e949e84bc1ea1442c5c79b7961edc5e6e
lettuce <lettuce.lu@lettucelu.com>
2021-10-20 14:13:27 +0800

    feat:modify node_modules/whatwg-url/dist/encoding.js

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+
+// connect to the mongoDB
+mongoose.connect('mongodb://localhost/todo-list')
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 
 // setting up homepage route
 app.get('/', (req, res) => {
