89f44b272def1936adc9f3e9b5b662a1af074cfd
lettuce <lettuce.lu@lettucelu.com>
2021-10-21 16:40:41 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser')
 // 載入 method-override
 const methodOverride = require('method-override')
 
+const routes = require('./routes')
 
 const Todo = require('./models/todo');
 const todo = require('./models/todo');
@@ -13,10 +14,12 @@ const app = express()
 
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(routes)
 // connect to the mongoDB
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -31,88 +34,10 @@ db.once('open', () => {
 })
 
 
-// setting up homepage route
-app.get('/', (req, res) => {
-  // res.send('hello world')
-
-  Todo.find()
-    .lean()
-    .sort({ _id: 'asc' })
-    .then(todos => res.render('index', { todos }))
-    .catch(error => console.log('error'))
 
 
-})
 
 
-app.get('/todos/new', (req, res) => {
-  res.render('new')
-})
-
-app.post('/todos', (req, res) => {
-  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
-  return Todo.create({ name })     // 存入資料庫
-    .then(() => res.redirect('/')) // 新增完成後導回首頁
-    .catch(error => console.log(error))
-})
-
-
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('detail', { todo }))
-    .catch(error => console.log(error))
-})
-
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => {
-      res.render('edit', { todo })
-    })
-    .catch(error => {
-      console.log(error)
-    })
-
-})
-
-// 更新功能
-app.put('/todos/:id', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => {
-      res.redirect(`/todos/${id}`)
-    })
-    .catch(error => {
-      console.log(error)
-    })
-
-})
-
-// 刪除功能
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id
-
-  // 查詢該筆資料
-  return Todo.findById(id)
-    .then(todo => {
-      todo.remove()
-    })
-    .then(() => {
-      res.redirect('/')
-    })
-    .catch(error => {
-      console.log(error)
-    })
-})
 
 
 // setting up port 3000 for service
