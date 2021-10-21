0f608a692e739a328f6cf57f1c16380285d2a10e
lettuce <lettuce.lu@lettucelu.com>
2021-10-21 15:44:17 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.get('/', (req, res) => {
 
   Todo.find()
     .lean()
+    .sort({ _id: 'asc' })
     .then(todos => res.render('index', { todos }))
     .catch(error => console.log('error'))
 
