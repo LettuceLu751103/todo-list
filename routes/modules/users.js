712b647c1d5a4690495a764de43510eccc2d6d4c
lettuce <lettuce.lu@lettucelu.com>
2021-10-22 19:21:46 +0800

    feat: add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -13,4 +14,31 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  console.log(name, email, password, confirmPassword)
+  User.findOne({ email }).then(user => {
+    if (user) {
+      console.log('user already exist!')
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    } else {
+      return User.create({
+        name,
+        email,
+        password
+      })
+        .then(() => {
+          res.redirect('/')
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  })
+})
 module.exports = router
