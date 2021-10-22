bc22e5e01363a05366dfe86eb541cd3846fab05d
lettuce <lettuce.lu@lettucelu.com>
2021-10-22 23:22:20 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -8,12 +8,11 @@ const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser')
 // 載入 method-override
 const methodOverride = require('method-override')
-
+const flash = require('connect-flash')   // 引用套件
 
 
 const routes = require('./routes')
 
-const Todo = require('./models/todo');
 const app = express()
 
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
@@ -36,10 +35,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 usePassport(app)
 
+app.use(flash())  // 掛載套件
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -24,10 +24,26 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
   console.log(name, email, password, confirmPassword)
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
   User.findOne({ email }).then(user => {
     if (user) {
-      console.log('user already exist!')
+      errors.push({ message: '這個 Email 已經註冊過了。' })
       res.render('register', {
         name,
         email,
@@ -54,6 +70,7 @@ router.post('/register', (req, res) => {
 // logout function
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
